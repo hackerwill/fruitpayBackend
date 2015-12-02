018f57c62a676e4215ce23a9420e7fd86f176578
weichou1229@gmail.com <kk2218669>
2015-12-02 18:48:08 +0800

    'fruitpay_startup'

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ var gls = require('gulp-live-server');
 
 
 gulp.task('server', function(){
-	var server = gls.static('build',8888);	//他會自己去找到對應的webapp底下的index.html
+	var server = gls.static('src/main/webapp',8888);	//他會自己去找到對應的webapp底下的index.html
 	server.start();
 	//頁面綁上<script src="//localhost:35729/livereload.js"></script>
 	//當檔案變更時可以觸發browser reload
-	gulp.watch(config.getAllPath(), function (file) {
+	gulp.watch('src/main/webapp/**/*.*', function (file) {
 		server.notify.apply(server, [file]);
 	});
 });
