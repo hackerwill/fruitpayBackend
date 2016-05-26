4154927ab0140ca81e602a957a6b5a8e7d685724
Will Hsu <will.g@mig.me>
2016-05-26 23:48:31 +0800

    Change tab to double white space and modify validFlag constantoption

--- app/constant/editConstant.controller.js
+++ app/constant/editConstant.controller.js
@@ -1,51 +1,51 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('constant')
-		.controller('EditConstantController', EditConstantController);
+  angular
+    .module('constant')
+    .controller('EditConstantController', EditConstantController);
 
-	EditConstantController.$inject = ['$mdDialog', 'constant', 'ConstantService' ];
+  EditConstantController.$inject = ['$mdDialog', 'constant', 'ConstantService' ];
 
-	function EditConstantController($mdDialog, constant, ConstantService) {
-		var vm = this;
-		vm.constant = angular.copy(constant);
+  function EditConstantController($mdDialog, constant, ConstantService) {
+    var vm = this;
+    vm.constant = angular.copy(constant);
 
-		vm.save = save;
-		vm.closeDialog = closeDialog;
+    vm.save = save;
+    vm.closeDialog = closeDialog;
 
-		//儲存Constant
-		function save() {
-			if(constant.constId){
-				updateConstant();
-			}else{
-				createConstant();
-			}
-		}
+    //儲存Constant
+    function save() {
+      if(constant.constId){
+        updateConstant();
+      }else{
+        createConstant();
+      }
+    }
 
-		//關閉Dialog
-		function closeDialog() {
-			$mdDialog.cancel();
-		}
+    //關閉Dialog
+    function closeDialog() {
+      $mdDialog.cancel();
+    }
 
-		//更新Constant
-		function updateConstant() {
-			ConstantService
-				.updateConstant(vm.constant)
-				.then(function(res) {
-					$mdDialog.hide(res);
-				});
-		}
+    //更新Constant
+    function updateConstant() {
+      ConstantService
+        .updateConstant(vm.constant)
+        .then(function(res) {
+          $mdDialog.hide(res);
+        });
+    }
 
-		//新增Constant
-		function createConstant() {
-			ConstantService
-				.createConstant(vm.constant)
-				.then(function(res) {
-					$mdDialog.hide(res);
-				});
-		}
+    //新增Constant
+    function createConstant() {
+      ConstantService
+        .createConstant(vm.constant)
+        .then(function(res) {
+          $mdDialog.hide(res);
+        });
+    }
 
-	}
+  }
 
 })();
