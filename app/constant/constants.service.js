49c08758c9da9051c8c9330b48996a7d4b34461f
Will Hsu <will.g@mig.me>
2016-05-26 23:48:31 +0800

    Change tab to double white space and modify validFlag constantoption

--- app/constant/constants.service.js
+++ app/constant/constants.service.js
@@ -1,40 +1,40 @@
 (function(){
-	'use strict';
+  'use strict';
 
-	angular
-		.module('constant')
-		.service('ConstantService',ConstantService);
+  angular
+    .module('constant')
+    .service('ConstantService',ConstantService);
 
-	ConstantService.$inject = ['$http','fruitpay'];
+  ConstantService.$inject = ['$http','fruitpay'];
 
-	function ConstantService($http, fruitpay) {
-		this.getAllAdminConstants = function(page, size) {
-			return $http.get(fruitpay+'staticDataCtrl/adminConstant?page='+page+'&size='+size);
-		}
+  function ConstantService($http, fruitpay) {
+    this.getAllAdminConstants = function(page, size) {
+      return $http.get(fruitpay+'staticDataCtrl/adminConstant?page='+page+'&size='+size);
+    }
 
-		this.getAdminConstantByConstId = function(constId) {
-			return $http.get(fruitpay+'staticDataCtrl/adminConstant/'+constId);
-		}
+    this.getAdminConstantByConstId = function(constId) {
+      return $http.get(fruitpay+'staticDataCtrl/adminConstant/'+constId);
+    }
 
-		this.updateConstant = function(constant) {
-			return $http.put(fruitpay+'staticDataCtrl/adminConstant',constant);
-		}
+    this.updateConstant = function(constant) {
+      return $http.put(fruitpay+'staticDataCtrl/adminConstant',constant);
+    }
 
-		this.createConstant = function(constant) {
-			return $http.post(fruitpay+'staticDataCtrl/adminConstant',constant);
-		}
+    this.createConstant = function(constant) {
+      return $http.post(fruitpay+'staticDataCtrl/adminConstant',constant);
+    }
 
-		this.getAllConstantOptionsByConstId = function (page, size, constId) {
-			return $http.get(fruitpay+'staticDataCtrl/adminConstantOption/'+constId+'/?page='+page+'&size='+size);
-		}
+    this.getAllConstantOptionsByConstId = function (page, size, constId) {
+      return $http.get(fruitpay+'staticDataCtrl/adminConstantOption/'+constId+'/?page='+page+'&size='+size);
+    }
 
-		this.updateConstantOption = function(constantOption) {
-			return $http.put(fruitpay+'staticDataCtrl/adminConstant/constOption',constantOption);
-		}
+    this.updateConstantOption = function(constantOption) {
+      return $http.put(fruitpay+'staticDataCtrl/adminConstant/constOption',constantOption);
+    }
 
-		this.createConstantOption = function(constantOption) {
-			return $http.post(fruitpay+'staticDataCtrl/adminConstant/constOption',constantOption);
-		}
-	}
+    this.createConstantOption = function(constantOption) {
+      return $http.post(fruitpay+'staticDataCtrl/adminConstant/constOption',constantOption);
+    }
+  }
 
 })();
--- app/constant/editConstantOptionDialog.controller.js
+++ app/constant/editConstantOptionDialog.controller.js
@@ -1,53 +1,51 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('constant')
-		.controller('EditConstantOptionController', EditConstantOptionController)
+  angular
+    .module('constant')
+    .controller('EditConstantOptionController', EditConstantOptionController)
 
-		EditConstantOptionController.$inject = ['constantOption', 'constant', '$mdDialog', 'ConstantService'];
+    EditConstantOptionController.$inject = ['constantOption', 'constant', '$mdDialog', 'ConstantService', 'UtilService'];
 
-		function EditConstantOptionController(constantOption, constant, $mdDialog, ConstantService){
+    function EditConstantOptionController(constantOption, constant, $mdDialog, ConstantService, UtilService){
 
-			var vm = this;
-			vm.constantOption = angular.copy(constantOption);
-			vm.constantOption.constant = angular.copy(constant);
-			vm.validFlags = [
-				{name: '有效',validFlagStatus: 1},
-				{name: '無效',validFlagStatus: 0}
-			];
+      var vm = this;
+      vm.constantOption = angular.copy(constantOption);
+      vm.constantOption.constant = angular.copy(constant);
+      vm.closeDialog = closeDialog;
+      vm.save = save;
 
-			vm.closeDialog = closeDialog;
-			vm.save = save;
+      UtilService.getConstant(15)
+        .then(function(result){
+          vm.validFlag = result.data;
+        });
 
+      function closeDialog() {
+        $mdDialog.cancel();
+      }
 
+      function save() {
+        if(vm.constantOption.optionId){
+          updateConstantOption();
+        }else{
+          createConstantOption();
+        }
+      }
 
-			function closeDialog() {
-				$mdDialog.cancel();
-			}
+      function updateConstantOption() {
+        ConstantService
+          .updateConstantOption(vm.constantOption)
+          .then(function(res) {
+            $mdDialog.hide(res);
+          });
+      }
 
-			function save() {
-				if(vm.constantOption.optionId){
-					updateConstantOption();
-				}else{
-					createConstantOption();
-				}
-			}
-
-			function updateConstantOption() {
-				ConstantService
-					.updateConstantOption(vm.constantOption)
-					.then(function(res) {
-						$mdDialog.hide(res);
-					});
-			}
-
-			function createConstantOption() {
-				ConstantService
-					.createConstantOption(vm.constantOption)
-					.then(function(res) {
-						$mdDialog.hide(res);
-					});
-			}
-		}
+      function createConstantOption() {
+        ConstantService
+          .createConstantOption(vm.constantOption)
+          .then(function(res) {
+            $mdDialog.hide(res);
+          });
+      }
+    }
 })();
