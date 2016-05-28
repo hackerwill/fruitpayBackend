41b75ccb46a707efdcb7e5270e956f4e9cf7133e
Will Hsu <will.g@mig.me>
2016-05-28 16:11:40 +0800

    Remove unneccesary ng-loader code

--- app/app.module.js
+++ app/app.module.js
@@ -2,116 +2,116 @@
     angular.element(document).ready(function () {
         angular.bootstrap(document, ['app']);
     });
-	
-	angular
-		.module('app',[
-		   			'ngRoute','ngAnimate','ngMaterial','ngMessages','ngAria','md.data.table',
-					'order','customer','util','coupon','login', 'shipment', 'angularjs-datetime-picker', 
-					'constant'
-		])
-		.constant("fruitpay", "${GULP_SERVER_DOMAIN}")
-		.directive('moDateInput', function ($window) {
-		    return {
-		        require:'^ngModel',
-		        restrict:'A',
-		        link:function (scope, elm, attrs, ctrl) {
-		            var moment = $window.moment;
-		            var dateFormat = attrs.moDateInput;
-		            attrs.$observe('moDateInput', function (newValue) {
-		                if (dateFormat == newValue || !ctrl.$modelValue) return;
-		                dateFormat = newValue;
-		                ctrl.$modelValue = new Date(ctrl.$setViewValue);
-		            });
+  
+  angular
+    .module('app',[
+             'ngRoute','ngAnimate','ngMaterial','ngMessages','ngAria','md.data.table',
+          'order','customer','util','coupon','login', 'shipment', 'angularjs-datetime-picker', 
+          'constant'
+    ])
+    .constant("fruitpay", "${GULP_SERVER_DOMAIN}")
+    .directive('moDateInput', function ($window) {
+        return {
+            require:'^ngModel',
+            restrict:'A',
+            link:function (scope, elm, attrs, ctrl) {
+                var moment = $window.moment;
+                var dateFormat = attrs.moDateInput;
+                attrs.$observe('moDateInput', function (newValue) {
+                    if (dateFormat == newValue || !ctrl.$modelValue) return;
+                    dateFormat = newValue;
+                    ctrl.$modelValue = new Date(ctrl.$setViewValue);
+                });
 
-		            ctrl.$formatters.unshift(function (modelValue) {
-		                if (!dateFormat || !modelValue) return "";
-		                var retVal = moment(modelValue).format(dateFormat);
-		                return retVal;
-		            });
+                ctrl.$formatters.unshift(function (modelValue) {
+                    if (!dateFormat || !modelValue) return "";
+                    var retVal = moment(modelValue).format(dateFormat);
+                    return retVal;
+                });
 
-		            ctrl.$parsers.unshift(function (viewValue) {
-		                var date = moment(viewValue, dateFormat);
-		                return (date && date.isValid() && date.year() > 1950 ) ? date.toDate() : "";
-		            });
-		        }
-		    };
-		})
-		.config(Config)
-		.run(run);
+                ctrl.$parsers.unshift(function (viewValue) {
+                    var date = moment(viewValue, dateFormat);
+                    return (date && date.isValid() && date.year() > 1950 ) ? date.toDate() : "";
+                });
+            }
+        };
+    })
+    .config(Config)
+    .run(run);
 
-	Config.$inject = ['$routeProvider', '$httpProvider'];
+  Config.$inject = ['$routeProvider', '$httpProvider'];
 
-	function Config($routeProvider, $httpProvider) {
-		$routeProvider.otherwise('/orders');
-		
-		//轉換日期格式從字串轉為日期
-		$httpProvider.defaults.transformResponse.push(function(responseData){
-			convertDateStringsToDates(responseData);
-			return responseData;
-		});
+  function Config($routeProvider, $httpProvider) {
+    $routeProvider.otherwise('/orders');
+    
+    //轉換日期格式從字串轉為日期
+    $httpProvider.defaults.transformResponse.push(function(responseData){
+      convertDateStringsToDates(responseData);
+      return responseData;
+    });
 
-		$httpProvider.interceptors.push('HttpInterceptor');
-	}
-	
-	run.$inject = ['$rootScope', '$location', '$timeout', 'AuthenticationService'];
-	function run( $rootScope, $location, $timeout, AuthenticationService) {
-		/**
-		 *  redirect to login page if not logged in and trying to access a restricted page
-		 */
-		$rootScope.$on('$routeChangeStart', function (event, next, current) {
-			
-			AuthenticationService.validateAccount()
-				.then(function(result){
-					var loggedIn = result;
-					if (!loggedIn) {
-						$timeout(function () {				
-							$location.path("/login");
-						});
-					}
-				});
-		});
-		
-		$rootScope.$on('$routeChangeSuccess', function(event, next, current) {
-			delete $rootScope.previousState;
-			delete $rootScope.currentState;
-			if(current && current.originalPath.indexOf("/logout") == -1)
-				$rootScope.previousState = current;
-			$rootScope.currentState = next;
-		});
-	}
-	
-	var regexIso8601 = re = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+    $httpProvider.interceptors.push('HttpInterceptor');
+  }
+  
+  run.$inject = ['$rootScope', '$location', '$timeout', 'AuthenticationService'];
+  function run( $rootScope, $location, $timeout, AuthenticationService) {
+    /**
+     *  redirect to login page if not logged in and trying to access a restricted page
+     */
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+      
+      AuthenticationService.validateAccount()
+        .then(function(result){
+          var loggedIn = result;
+          if (!loggedIn) {
+            $timeout(function () {        
+              $location.path("/login");
+            });
+          }
+        });
+    });
+    
+    $rootScope.$on('$routeChangeSuccess', function(event, next, current) {
+      delete $rootScope.previousState;
+      delete $rootScope.currentState;
+      if(current && current.originalPath.indexOf("/logout") == -1)
+        $rootScope.previousState = current;
+      $rootScope.currentState = next;
+    });
+  }
+  
+  var regexIso8601 = re = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
 
-	function convertDateStringsToDates(input) {
-		// Ignore things that aren't objects.
-		if (typeof input !== "object") return input;
+  function convertDateStringsToDates(input) {
+    // Ignore things that aren't objects.
+    if (typeof input !== "object") return input;
 
-		for (var key in input) {
-			if (!input.hasOwnProperty(key)) continue;
+    for (var key in input) {
+      if (!input.hasOwnProperty(key)) continue;
 
-			var value = input[key];
-			var match;
-			// Check for string properties which look like dates.
-			// TODO: Improve this regex to better match ISO 8601 date strings.
-			if (typeof value === "string" && checkDateNameConstrants(key) && (match = value.match(regexIso8601))) {
-				// Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
-				var milliseconds = Date.parse(match[0]);
-				if (!isNaN(milliseconds) ) {
-					input[key] = new Date(milliseconds);
-				}
-			} else if(checkDateNameConstrants(key)
-				&& !isNaN(value)){
-				input[key] = new Date(value);
-			}else if (typeof value === "object") {
-				// Recurse into object
-				convertDateStringsToDates(value);
-			}
-		}
-	}
-	
-	function checkDateNameConstrants(key){
-		//限制key結尾包含 date 或 time 或 day 則檢查型態才能轉成日期
-		return key.toLowerCase().endsWith('day') || key.toLowerCase().endsWith('date') || key.toLowerCase().endsWith('time');
-	}
-	
+      var value = input[key];
+      var match;
+      // Check for string properties which look like dates.
+      // TODO: Improve this regex to better match ISO 8601 date strings.
+      if (typeof value === "string" && checkDateNameConstrants(key) && (match = value.match(regexIso8601))) {
+        // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+        var milliseconds = Date.parse(match[0]);
+        if (!isNaN(milliseconds) ) {
+          input[key] = new Date(milliseconds);
+        }
+      } else if(checkDateNameConstrants(key)
+        && !isNaN(value)){
+        input[key] = new Date(value);
+      }else if (typeof value === "object") {
+        // Recurse into object
+        convertDateStringsToDates(value);
+      }
+    }
+  }
+  
+  function checkDateNameConstrants(key){
+    //限制key結尾包含 date 或 time 或 day 則檢查型態才能轉成日期
+    return key.toLowerCase().endsWith('day') || key.toLowerCase().endsWith('date') || key.toLowerCase().endsWith('time');
+  }
+  
 })();
--- app/shipment/shipmentPreview.controller.js
+++ app/shipment/shipmentPreview.controller.js
@@ -1,112 +1,112 @@
 (function(){
-	'use strict';
-	angular
-		.module('shipment')
-		.controller('ShipmentPreviewController', ShipmentPreviewController);
-	ShipmentPreviewController.$inject = ['OrderService', '$location' ,'ShipmentService', '$mdDialog', '$scope', '$q', 'FileSaverService', 'LogService', 'UtilService'] ;
-	function ShipmentPreviewController(OrderService, $location, ShipmentService, $mdDialog, $scope, $q, FileSaverService, LogService, UtilService){
-		var vm = this ;	//view model
-		vm.selected = [] ;
-		vm.resource = {totalElements:0,size: 100,number: 1};//md-table-pagination的初始值
-		vm.pageOptions = [100, 500, 1000];
-		vm.condition = {};
-		
-		vm.pagination = pagination;
-		vm.search =search;
-		vm.openEditOrderDialog = openEditOrderDialog;
-		vm.moveToShipmentChange = moveToShipmentChange;
-		vm.update = update;
+  'use strict';
+  angular
+    .module('shipment')
+    .controller('ShipmentPreviewController', ShipmentPreviewController);
+  ShipmentPreviewController.$inject = ['OrderService', '$location' ,'ShipmentService', '$mdDialog', '$scope', '$q', 'FileSaverService', 'LogService', 'UtilService'] ;
+  function ShipmentPreviewController(OrderService, $location, ShipmentService, $mdDialog, $scope, $q, FileSaverService, LogService, UtilService){
+    var vm = this ;  //view model
+    vm.selected = [] ;
+    vm.resource = {totalElements:0,size: 100,number: 1};//md-table-pagination的初始值
+    vm.pageOptions = [100, 500, 1000];
+    vm.condition = {};
+    
+    vm.pagination = pagination;
+    vm.search =search;
+    vm.openEditOrderDialog = openEditOrderDialog;
+    vm.moveToShipmentChange = moveToShipmentChange;
+    vm.update = update;
     vm.exportFile = exportFile;
-		
-		activate();
+    
+    activate();
 
-		$q.all([
-			//得到所有訂單狀態
-			UtilService.getAllOrderStatus()
-				.then(function(result){
-					vm.orderStatuses = result.data;
-				}),
-			//得到所有運送週期
-			UtilService.getAllShipmentPeriods()
-				.then(function(result){
-					vm.shipmentPeriods = result.data;
-				}), 
-			//得到配送日
-			UtilService.getConstant(6)
-				.then(function(result){
-					vm.deliveryDay = result.data;
-				}),
+    $q.all([
+      //得到所有訂單狀態
+      UtilService.getAllOrderStatus()
+        .then(function(result){
+          vm.orderStatuses = result.data;
+        }),
+      //得到所有運送週期
+      UtilService.getAllShipmentPeriods()
+        .then(function(result){
+          vm.shipmentPeriods = result.data;
+        }), 
+      //得到配送日
+      UtilService.getConstant(6)
+        .then(function(result){
+          vm.deliveryDay = result.data;
+        }),
       //得到有效無效
       UtilService.getConstant(15)
         .then(function(result){
           vm.validFlag = result.data;
         }),
-		]).then(function(){console.log("finished.")});
+    ]).then(function(){console.log("finished.")});
 
-		function activate(){
-			pagination(vm.resource.number, vm.resource.size);
-		}
-		//location='#/orders/'+id;
-		function pagination(page,size){
-			vm.selected = [] ;
-			
-			ShipmentService.shipmentPreview(page-1, size, vm.condition)
-				.then(function(result){
-					console.log(result);
-					if(result && result.data != ''){
-						result.data.customerOrders.number = result.data.customerOrders.number+1;
-						vm.resource = result.data.customerOrders;
-					}else{
-						vm.resource = result.data.customerOrders;
-					}
-          			vm.orderIds = result.data.orderIds;
-				});
-		}
+    function activate(){
+      pagination(vm.resource.number, vm.resource.size);
+    }
+    //location='#/orders/'+id;
+    function pagination(page,size){
+      vm.selected = [] ;
+      
+      ShipmentService.shipmentPreview(page-1, size, vm.condition)
+        .then(function(result){
+          console.log(result);
+          if(result && result.data != ''){
+            result.data.customerOrders.number = result.data.customerOrders.number+1;
+            vm.resource = result.data.customerOrders;
+          }else{
+            vm.resource = result.data.customerOrders;
+          }
+                vm.orderIds = result.data.orderIds;
+        });
+    }
 
-		function search(){
-			pagination(vm.resource.number, vm.resource.size);
-		}
+    function search(){
+      pagination(vm.resource.number, vm.resource.size);
+    }
 
 
-		function moveToShipmentChange(order){
-			OrderService.setOrder(order);
-			$location.path("/order/shipmentChange/" + order.orderId);
-		}
+    function moveToShipmentChange(order){
+      OrderService.setOrder(order);
+      $location.path("/order/shipmentChange/" + order.orderId);
+    }
 
-		function openEditOrderDialog($event ,order){
-			$mdDialog.show({
-				targetEvent: $event,
-				hasBackdrop: true,
-				clickOutsideToClose :true,
-				locals: { order: order },
-				templateUrl : 'app/order/editOrderDialog.html',
-				controller:'EditOrderController as vm'
-		       }).then(function(res){
-		    	   
-		    	   if(order.orderId){
-		    		   updateOrder(res);
-		    	   }
-		    	   
-		       });
-		}
+    function openEditOrderDialog($event ,order){
+      $mdDialog.show({
+        targetEvent: $event,
+        hasBackdrop: true,
+        clickOutsideToClose :true,
+        locals: { order: order },
+        templateUrl : 'app/order/editOrderDialog.html',
+        controller:'EditOrderController as vm'
+           }).then(function(res){
+             
+             if(order.orderId){
+               updateOrder(res);
+             }
+             
+           });
+    }
 
-		/**更新客戶**/
-	    function update(order){
-	    	OrderService.update(order).then(function(res){
-	    		if(res)
-					LogService.showSuccess('更新成功');
-	    	});
-	    }
+    /**更新客戶**/
+      function update(order){
+        OrderService.update(order).then(function(res){
+          if(res)
+          LogService.showSuccess('更新成功');
+        });
+      }
 
-		/**客戶更新後 替換掉原本list上的customer object**/
-		function updateOrder(order){
-			console.log('update',order);
-			angular.forEach(vm.resource.content, function(value, key) {
-				if(vm.resource.content[key].orderId == order.orderId){
-					vm.resource.content[key] = order ;
-				} 
-			});	
-		}
+    /**客戶更新後 替換掉原本list上的customer object**/
+    function updateOrder(order){
+      console.log('update',order);
+      angular.forEach(vm.resource.content, function(value, key) {
+        if(vm.resource.content[key].orderId == order.orderId){
+          vm.resource.content[key] = order ;
+        } 
+      });  
+    }
 
     function exportFile(){
       var deferred = $q.defer();
@@ -126,7 +126,7 @@
       FileSaverService.saveAs(blob, filename);
     }
 
-	}
+  }
 
 })();
 
