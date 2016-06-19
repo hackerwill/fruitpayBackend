c4f255ff7cb6c818c4cfb0e9c55400d908e812b8
wowmama <wowmama0107@gmail.com>
2016-06-19 14:50:22 +0800

    Add cache static data

--- app/util/util.service.js
+++ app/util/util.service.js
@@ -6,66 +6,116 @@
         .service('UtilService',UtilService);
     UtilService.$inject = ['$q','$http','fruitpay'] ;
     function UtilService($q,$http,fruitpay){
-    	this.getAllPostalCodes = function(){
+    	var allPostalCodes;
+        var allOrderStatus;
+        var allProducts;
+        var allOrderPlatforms;
+        var allShipmentPeriods;
+        var allPaymentModes;
+        var allOrderPrograms;
+
+
+        this.getAllPostalCodes = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/getAllPostalCodes')
-        		.then(function(res){
-        			resolve(res);
-        		});
+              if(!allPostalCodes){
+                $http
+                  .get(fruitpay+'staticDataCtrl/getAllPostalCodes')
+                  .then(function(res){
+                    allPostalCodes = res;
+                    resolve(allPostalCodes);
+                  });
+              }else{
+                resolve(allPostalCodes);
+              }
             });
     	}
 		
 		this.getAllOrderStatus = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/orderStatuses')
-        		.then(function(res){
-        			resolve(res);
-        		});
+              if(!allOrderStatus){
+                $http.get(fruitpay+'staticDataCtrl/orderStatuses')
+                .then(function(res){
+                  allOrderStatus = res;
+                  resolve(res);
+                });
+              }else{
+                resolve(allOrderStatus);
+              }
             });
     	}
 		
 		this.getAllProducts = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/getAllProducts')
-        		.then(function(res){
-        			resolve(res);
-        		});
+                if(!allProducts){
+                  $http.get(fruitpay+'staticDataCtrl/getAllProducts')
+                  .then(function(res){
+                    allProducts = res;
+                    resolve(res);
+                  });
+                }else{
+                  resolve(allProducts);
+                }
+        		
             });
     	}
 		
 		this.getAllOrderPlatforms = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/orderPlatforms')
-        		.then(function(res){
-        			resolve(res);
-        		});
+                if(!allOrderPlatforms){
+                    $http.get(fruitpay+'staticDataCtrl/orderPlatforms')
+                    .then(function(res){
+                        allOrderPlatforms = res;
+                        resolve(res);
+                    });
+                }else{
+                  resolve(allOrderPlatforms);
+                }
+        		
             });
     	}
 		
 		this.getAllShipmentPeriods = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/shipmentPeriods')
-        		.then(function(res){
-        			resolve(res);
-        		});
+                if(!allShipmentPeriods){
+                    $http.get(fruitpay+'staticDataCtrl/shipmentPeriods')
+                    .then(function(res){
+                        allShipmentPeriods = res;
+                        resolve(res);
+                    });
+                }else{
+                  resolve(allShipmentPeriods);
+                }
+        		
             });
     	}
 		
 		this.getAllPaymentModes = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/paymentModes')
-        		.then(function(res){
-        			resolve(res);
-        		});
+                if(!allPaymentModes){
+                    $http.get(fruitpay+'staticDataCtrl/paymentModes')
+                    .then(function(res){
+                        allPaymentModes = res;
+                        resolve(res);
+                    });
+                }else{
+                  resolve(allPaymentModes);
+                }
+        		
             });
     	}
 		
 		this.getAllOrderPrograms = function(){
             return $q(function(resolve, reject){
-        		$http.get(fruitpay+'staticDataCtrl/orderPrograms')
-        		.then(function(res){
-        			resolve(res);
-        		});
+                if(!allOrderPrograms){
+                    $http.get(fruitpay+'staticDataCtrl/orderPrograms')
+                    .then(function(res){
+                        allOrderPrograms = res;
+                        resolve(res);
+                    });
+                }else{
+                  resolve(allOrderPrograms);
+                }
+        		
             });
     	}
 		
